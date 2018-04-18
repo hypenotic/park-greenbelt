1020abc20a0f6cda5333008f85db65fabea47480
Emily Dela Cruz <delacruz.emily@gmail.com>
2018-04-18 16:19:50 -0400

    Sets up base

---
+++ src/components/Footer.vue
@@ -0,0 +1,227 @@
+<template>
+    <div>  
+        <footer v-if="this.$route.path == '/faq'">
+            <div>
+                <div class="columns">
+                    <div class="column is-6" style="background-color: green; color: white;">
+                        <div class="columns is-centered">
+                            <div class="column is-4">
+                                <!-- <p>Founding Sponsor</p>
+                                <img src="https://parkpeople.ca/listings/custom/uploads/2017/09/TD-logo.png"> -->
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column is-6" style="background-color: green; color: white;">
+                        <div class="columns">
+                            <div class="column is-6">
+                                <ul>
+                                    <li style="color: white;font-weight: bold;">Park People</li>
+                                    <li>401 Richmond Street West</li>
+                                    <li>Studio 119, City Builder’s Lab</li>
+                                    <li>Toronto, ON M5V 3A8</li>
+                                </ul>
+                                <ul class="details">
+                                    <li>Phone: <a href="tel:+14165835776">416-583-5776</a></li>
+                                    <li>Email: <a href="mailto:info@parkpeople.ca">info@parkpeople.ca</a></li>
+                                    <li>Charity #: 846449437RR0001</li>
+                                </ul>
+                                <ul class="social-links">
+                                    <li><a href="https://parkpeople.ca/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                                    <li><a href="https://twitter.com/Park_People" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                                    <li><a href="https://www.facebook.com/ParkPeopleTO/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                </ul>
+    
+                            </div>
+                            <div class="column is-6">
+                                <a class="button" href="https://parkpeople.ca/donate-now" target="_blank">Donate</a>
+                            </div>
+    
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer> 
+        <footer v-else class="footer-grants">
+            <div>
+                <div class="columns">
+                    <div class="column is-6" style="background-color: green; color: white;">
+                        <div class="columns is-centered">
+                            <div class="column is-4">
+                                <!-- <p>Founding Sponsor</p>
+                                <img src="https://parkpeople.ca/listings/custom/uploads/2017/09/TD-logo.png"> -->
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column is-6" style="background-color: green; color: white;">
+                        <div class="columns">
+                            <div class="column is-6">
+                                <ul>
+                                    <li style="color: white;font-weight: bold;">Park People</li>
+                                    <li>401 Richmond Street West</li>
+                                    <li>Studio 119, City Builder’s Lab</li>
+                                    <li>Toronto, ON M5V 3A8</li>
+                                </ul>
+                                <ul class="details">
+                                    <li>Phone: <a href="tel:+14165835776">416-583-5776</a></li>
+                                    <li>Email: <a href="mailto:info@parkpeople.ca">info@parkpeople.ca</a></li>
+                                    <li>Charity #: 846449437RR0001</li>
+                                </ul>
+                                <ul class="social-links">
+                                    <li><a href="https://parkpeople.ca/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                                    <li><a href="https://twitter.com/Park_People" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                                    <li><a href="https://www.facebook.com/ParkPeopleTO/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                </ul>
+    
+                            </div>
+                            <div class="column is-6">
+                                <a class="button" href="https://parkpeople.ca/donate-now" target="_blank">Donate</a>
+                            </div>
+    
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer> 
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+    // Import Variables
+    @import '../styles/variables.scss';
+
+    .button {
+        color: $white;
+        background-color: $blue;
+        font-family: $family-sanserif;
+        font-weight: bold;
+        border: 2px solid $blue;
+        border-radius: 50px;
+        padding: 8px 24px;
+        &:hover {
+            border: 2px solid $blue;
+            background-color: $white;
+            color: $blue; 
+        }
+    }
+
+    footer {
+        font-size: 0.8rem;
+        p, ul, li {
+            font-size: 0.8rem;
+        }
+        ul + ul {
+            margin-top: 16px;
+        }
+    }
+
+    footer {
+        position: relative;
+        padding-top: 150px;
+        > div {
+            position: relative;
+            &:before {
+                content: '';
+                position: absolute;
+                left: 0;
+                top: -70px;
+                width: 100%;
+                height: 160px;
+                background: #fff;
+                -webkit-transform: skewY(5deg);
+                -moz-transform: skewY(5deg);
+                -ms-transform: skewY(5deg);
+                -o-transform: skewY(5deg);
+                transform: skewY(5deg);
+                -webkit-backface-visibility: hidden;
+            }
+        }
+    }
+
+    footer.footer-grants {
+        position: relative;
+        margin-top: 100px;
+        padding-top: 60px;
+        > div {
+            position: relative;
+            &:before {
+                content: '';
+                position: absolute;
+                left: 0;
+                top: -70px;
+                width: 100%;
+                height: 130px;
+                background: #fff;
+                @media #{$large-and-up} {
+                    -webkit-transform: skewY(5deg);
+                    -moz-transform: skewY(5deg);
+                    -ms-transform: skewY(5deg);
+                    -o-transform: skewY(5deg);
+                    transform: skewY(5deg);
+                }
+                @media #{$xlarge-and-up} {
+                    -webkit-transform: skewY(3deg);
+                    -moz-transform: skewY(3deg);
+                    -ms-transform: skewY(3deg);
+                    -o-transform: skewY(3deg);
+                    transform: skewY(3deg);
+                }
+            }
+        }
+    }
+
+    footer >div >div >div {
+        padding-top: 80px;
+        padding-left: 40px;
+        padding-right: 40px;
+        @media #{$medium-and-up} {
+           padding-top: 200px;
+        }
+    }
+    footer >div >div >div:first-child {
+        background: rgb(249, 249, 249);
+    }
+    footer >div >div >div:last-child {
+        padding: 40px;
+        @media #{$medium-and-up} {
+            padding: 200px 80px 80px;
+        }
+    }
+
+    footer img {
+        max-width: 150px;
+        height: auto;
+        padding-top: 0px;
+    }
+
+    .social-links {
+        li {
+            display: inline-block;
+            a {
+                color: white;
+                &:hover {
+                    opacity: 0.6;
+                }
+            }
+            i {
+                font-size: 32px;
+            }
+        }
+        li + li {
+            margin-left: 16px;
+        }
+    }
+    .details a {
+        color: white;
+        text-decoration: underline;
+        &:hover {
+            text-decoration: none;
+        }
+    }
+</style>
---
+++ src/views/Grants.vue
@@ -0,0 +1,604 @@
+<template>
+	<div v-if="loading==false">
+		<section class="section" v-if="data && data.hasOwnProperty('meta_box')">
+			<div class="container">
+				
+				<div class="topContent" v-html="data.content.rendered"></div>
+
+			</div>
+		</section>
+
+		<section class="more-info">
+			<div class="container">
+				<div v-html="data.meta_box._page_grant_more_info"></div>
+			</div>
+		</section>
+
+		<section class="grants-newsletter">
+			<div class="container">
+				<p style="margin-bottom: 1rem;">Want to stay up-to-date on Park People news?</p>
+				<a class="button" href="http://parkpeople.us2.list-manage.com/subscribe?u=ba963c8c64482c0ad756245c3&id=efc9b053b8" target="_blank">Get the Park People newsletter!</a>
+			</div>
+		</section>
+
+	</div>
+	<div v-else class="loading-panel">
+		<div>
+			<img src="https://parkpeople.ca/listings/custom/uploads/2018/01/birdflying_pp_small.gif" alt="">
+		</div>
+	</div>
+</template>
+
+<script>
+import axios from 'axios';
+// import { mapState } from 'vuex'
+export default {
+	data() {
+		return {
+			data: {},
+			relatedPosts: [],
+			errors: [],
+			loading: true
+		};
+	},
+	filters: {
+		removeHyphen(value){
+			return value.replace("-", ' ');
+		},
+		capitalizeFirstLetter(value) {
+			return value.charAt(0).toUpperCase() + value.slice(1);
+		},
+		toUppercase(value) {
+			return value.toUpperCase();
+		},
+		readMore(value, length, suffix) {
+			if (value.length < length)
+			return value;	
+			return value.substring(0, length) + suffix;
+		},
+		stripHTML(value) {
+			return value.replace(/(<([^>]+)>)/ig,"");
+		},
+		toTitleCase(value) {
+    		return value.replace(/\w\S*/g, (txt) => {
+				return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+			});
+		},
+	},
+	computed: {
+
+    },
+	methods: {
+
+	},
+	mounted() {
+
+	},
+	created() {
+		axios.get('https://parkpeople.ca/listings/wp-json/wp/v2/pages/1070?_embed')
+		.then(response => {
+            console.log(response.data)
+			this.data = response.data
+			
+			this.loading = false
+
+			axios.all([
+				axios.get('https://parkpeople.ca/listings/wp-json/wp/v2/case-study/?_embed&categories=133&per_page=20'),
+				axios.get('https://parkpeople.ca/listings/wp-json/wp/v2/research/?_embed&categories=133&per_page=20'),
+				axios.get('https://parkpeople.ca/listings/wp-json/wp/v2/resource/?_embed&categories=133&per_page=20')
+			])
+			.then(axios.spread((response, response1, response2) => {
+				// console.log(response.data)
+				let allPosts  = response.data.concat(response1.data, response2.data);
+				// console.log('old', allPosts)
+				allPosts.sort(function(a,b){
+					return new Date(b.date) - new Date(a.date)
+				})	
+				// console.log('sorted', allPosts)
+				this.relatedPosts = allPosts
+			}))
+			.catch(e => {
+				console.log(e)
+				this.errors.push(e)
+			})
+
+		})
+		.catch(e => {
+			console.log(e)
+			this.errors.push(e)
+		})
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+
+@import '../styles/variables.scss';
+
+.section {
+	background-color: #fff;
+	@media #{$small-and-down} {
+		padding: 24px 1.5rem !important;
+	}
+}
+
+.card-content {
+	padding: 0.8rem 1.5rem;
+}
+
+h1 {
+    color: $orange;
+	font-size: 35px;
+	line-height: 1.2;
+	font-weight: 700;
+	text-align: center;
+	font-family: 'Roboto Condensed', sans-serif;
+	text-transform: uppercase;
+	color: grey; 
+	margin: 0;
+	font-weight: 300;
+	@media #{$large-and-up} {
+		font-size: 30px;
+		line-height: 1.3;
+		font-weight: 700;
+		margin-top: 20px;
+		font-weight: 400;
+	}
+}
+
+h1.main-heading {
+	// font-size: 25px;
+}
+
+.intro-logo {
+	margin: 0 auto;
+	// max-width: 400px;
+	text-align: center;
+	img {
+		max-width: 150px;
+		height: auto;
+	}
+	span.top-logo-text {
+		font-family: 'Open Sans';
+		text-transform: uppercase;
+		color: grey; 
+		margin: 0;
+		font-weight: 800;
+		font-size: 35px;
+		// line-height: 1.3;
+	}
+	span.bottom-logo-text {
+		font-family: 'Roboto Condensed', sans-serif;
+		text-transform: uppercase;
+		color: $body-color;
+		font-weight: 400;
+		font-size: 25px; 
+		// line-height: 1.3;
+	}
+	h1 {
+		margin: 0;
+		line-height: 1;
+		margin-top: 20px;
+	}
+}
+
+.container {
+    max-width: 800px;
+    margin: 0 auto;
+}
+
+.loading-panel {
+	min-height: 100vh;
+	background: $white;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+
+.wide-container {
+    max-width: 1160px;
+    margin: 0 auto;
+}
+
+.content a h4 {
+	color: rgba(30,177,242, 1);
+	font-size: 1.2rem;
+	line-height: 1.7rem;
+	font-weight: 700;
+	margin-bottom: 0.3rem;
+	margin-top: 0.3rem;
+	// line-height: 1.4;
+	&:hover {
+		color: rgba(30,177,242,0.7);
+	}
+}
+
+.card .content {
+	p {
+		margin-bottom: 0.3rem;
+	}
+}
+
+.activity-list-container,
+.activity-list-container span,
+.activity-list-container strong {
+	font-size: 12px;
+	line-height: 1.5;
+	color: rgba(0,0,0,0.4) !important;
+}
+
+.activity-list-container span {
+	display:inline-block;
+	margin-right: 5px;
+	position: relative;
+	&:after {
+		content: ',';
+		position: absolute;
+		bottom: 0;
+		right: -2px;
+		color: rgba(0,0,0,0.4) !important;
+	}
+}
+
+.activity-list-container span:last-child {
+	margin-right: 0px;
+	&:after {
+		display: none;
+	}
+}
+
+.skewed-bg{
+  	background: #fff;
+ 	padding: 100px 0;
+	transform: skew(0deg, -5deg);
+  	margin-top: -130px;
+	position: relative;
+	z-index: -1;
+}
+
+img {
+	object-fit: cover;
+}
+
+.content {
+	p {
+		font-size: 0.8rem;
+		line-height: 1.3rem;
+	}
+	ul {
+		list-style-type: none;
+		margin: 0 0 10px 0;
+		li { 
+			color: rgba(0,0,0,0.5);
+			display: inline;
+			font-weight: 700;
+			&:not(:last-child):after {
+				content: " | ";
+			}
+		}
+	}
+}
+
+.topContent {
+    margin: 32px 0;
+	margin-bottom: 50px;
+    font-size: 1rem;
+	@media #{$medium-and-up} {
+		font-size: 1.2rem;
+    }
+    p {
+        font-size: 1.2rem;
+    }
+}
+
+.application-eligibility {
+	position: relative;
+	z-index: 500;
+	@media #{$small-and-down} {
+        div + div {
+			margin-top: 40px;
+		}
+	}
+	@media #{$large-and-up} {
+        display: flex;
+		justify-content: space-between;
+		>div {
+			width: 45%;
+			// margin: 0 1%;
+			h2 {
+				font-size: 1rem;
+				font-weight: 700;
+				font-family: $family-sanserif;
+				@media #{$medium-and-up} {
+					font-size: 1.2rem;
+				}
+			}
+		}
+	}
+}
+
+.application-eligibility ul {
+	li {
+		display: flex;
+		align-items: center;
+		margin-bottom: 16px;
+		font-size: 0.9rem;
+		line-height: 1.5;
+		img {
+			margin-right: 16px;
+			max-height: 40px;
+			width: auto;
+		}
+	}
+}
+
+.application-eligibility ol {
+	list-style: none;
+	>li {
+		counter-increment: item;
+		margin-bottom: 5px;
+		position: relative;
+		padding-left: 70px;
+		padding-bottom: 24px;
+		margin-bottom: 8px;
+		font-size: 0.9rem;
+		line-height: 1.5;
+	}
+	> li:first-child {
+		margin-top: 24px;
+	}
+	>li:not(:last-child) {
+		// border-bottom: 2px dashed $orange;
+	}
+	>li:before {
+		margin-right: 10px;
+		content: counter(item);
+		background: white;
+		border-radius: 100%;
+		border: 2px solid $orange;
+		color: $orange;
+		width: 40px;
+		height: 40px;
+		font-family: serif;
+		font-size: 24px;
+		font-style: italic;
+		font-weight: bold;
+		text-align: center;
+		padding-top: 0px;
+		// display: inline-block;
+		position: absolute;
+		top: 0;
+		left: 0px;
+	}
+}
+
+.align-center {
+	text-align: center;
+	position: relative;
+	z-index: 200;
+}
+
+.cta_button {
+	display: inline-block;
+	margin: 0 auto;
+	color: $white;
+	background-color: $orange;
+	border:4px solid $orange;
+	padding: 16px 40px;
+	border-radius: 50px;
+	font-size: 32px;
+	margin: 32px 0;
+	font-weight: bold;
+	&:hover {
+		background-color: $white;
+		color: $orange;
+		border:4px solid $orange;
+	}
+}
+
+.more-info {
+	position: relative;
+	z-index: 500;
+	p {
+		font-size: 1.5rem;
+	}
+}
+
+.grants-newsletter {
+	text-align: center;
+	font-size: 0.8rem;
+	line-height: 1.5;
+	margin-top: 40px;
+	p {
+		font-size: 0.8rem;
+		line-height: 1.5;
+	}
+}
+
+.grant-sponsors {
+	padding-top: 100px;
+	text-align: center;
+	font-size: 0.8rem;
+	line-height: 1.5;
+	p {
+		font-size: 0.8rem;
+		line-height: 1.5;
+	}
+	ul {
+		margin: 0 auto;
+		li {
+			display: inline-block;
+			margin: 0 24px;
+			img {
+				max-width: 100px;
+				height: auto;
+			}
+		}
+	}
+}
+
+.grants-newsletter {
+	.button {
+		background: $white;
+		border: 2px solid $blue;
+		color: $blue;
+		border-radius: 50px;
+		font-weight: bold;
+		padding: 16px 32px;
+		height: 50px;
+		@media #{$small-and-down} {
+			height: auto;
+			margin-top: 8px;
+			width: 80%;
+			white-space: normal;
+			display: block;
+			margin: 0 auto;
+		}
+		&:hover {
+			background: $blue;
+			border: 2px solid $blue;
+			color: $white;
+		}
+	}
+}
+
+
+.related-resources {
+	padding: 24px;
+	padding-bottom: 180px;
+	margin-top: 50px;
+	margin-bottom: -50px;
+	@media #{$medium-and-up} {
+		padding: 50px;
+		padding-bottom: 200px;
+	}
+	h3 {
+		color: $green;
+		font-size: 40px;
+		@media #{$small-and-down} {
+			font-size: 32px;
+			font-weight: bold;
+		}
+	}
+	a {
+        border-bottom: 1px solid $blue;
+        &:hover {
+            color: $blue;
+            font-weight: bold;
+        }
+    }
+}
+
+.related-resources-copy {
+	text-align: center;
+	margin-bottom: 24px;
+}
+
+@-webkit-keyframes slide {
+    from { background-position: 0 0; }
+    to { background-position: -400px 0; }
+}
+
+@keyframes slide {
+    from { background-position: 0 0; }
+    to { background-position: -200% 0; }
+}
+
+.grant-illustration {
+	position: relative;
+	margin-top: -200px;
+	z-index: 100;
+	padding-bottom: 15%;
+	min-height: 300px;
+	@media #{$medium-and-up} {
+		min-height: 300px;
+	}
+	@media #{$large-and-up} {
+		min-height: 500px;
+	}
+	.main-animation {
+		position: absolute;
+		left: 0;
+		top: 130px;
+		z-index: 20;
+		@media #{$medium-and-up} {
+			top: 15%;
+			width: 700px;
+			left: 50%;
+			margin-left: -350px;
+			// right: 50%;
+		}
+		@media #{$large-and-up} {
+			top: -15%;
+			width: 1300px;
+			left: 50%;
+			margin-left: -650px;
+			// right: 50%;
+		}
+		@media #{$xlarge-and-up} {
+			width: 1300px;
+			left: 50%;
+			margin-left: -650px;
+			// right: 50%;
+		}
+	}
+	.clouds {
+		position: absolute;
+		background: url('https://parkpeople.ca/listings/custom/uploads/2018/01/park_people_event_greenline_background_long.jpg') repeat 0 0;
+		width: 100%;
+		height: 500px;
+		background-size: cover;
+		left: 0;
+		top: 0%;
+		z-index: 10;
+		-webkit-animation: slide 40s linear infinite alternate;
+		animation: slide 40s linear infinite alternate;
+		@media #{$small-and-down} {
+			height: 350px;
+		}
+		@media #{$medium-and-up} {
+			height: 400px;
+		}
+		@media #{$large-and-up} {
+			height: 500px;
+		}
+		@media #{$xlarge-and-up} {
+			// width: 1000px;
+			background-size: contain;
+		}
+	}
+}
+
+#bird-anchor {
+	position: relative;
+	&:before {
+		content: url('https://parkpeople.ca/listings/custom/uploads/2018/01/birdflying_pp_small.gif');
+		position: absolute;
+		width: 50px;
+		height: 50px;
+		top: 100px;
+		left: -120px;
+		-moz-transform: scaleX(-1);
+        -o-transform: scaleX(-1);
+        -webkit-transform: scaleX(-1);
+	}
+	@media #{$xlarge-and-up} {
+        &:before {
+			content: url('https://parkpeople.ca/listings/custom/uploads/2018/01/birdflying_pp_small.gif');
+			position: absolute;
+			width: 50px;
+			height: 50px;
+			left: -120px;
+			top: 200px;
+		}
+	}
+}
+
+.card__activity-list {
+	ul {
+		li {
+			font-size: 0.8rem;
+		}
+	}
+}
+</style>
